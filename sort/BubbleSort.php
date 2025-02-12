1e2e65041b894c74d1d30b7313f5be394c88e761
Nguyễn Minh Châu <chaunm.146.2001@gmail.com>
2025-02-12 20:25:49 +0700

    bubble sort

---
+++ sort/BubbleSort.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\sort;
+
+require "./SortInterface.php";
+require "./SwapTrait.php";
+
+class BubbleSort implements SortInterface
+{
+    use SwapTrait;
+
+    public function sort(array &$array)
+    {
+        for ($i = 0; $i < count($array); $i++) {
+            for ($j = 0; $j < count($array) - $i - 1; $j++) {
+                if ($array[$j] > $array[$j + 1]) {
+                    $this->swap($array, $j, $j + 1);
+                }
+            }
+        }
+    }
+}
+
+$arr = [3,12,5,2,3,1,2];
+$bubbleSort = new BubbleSort();
+$bubbleSort->sort($arr);
+print_r($arr);
